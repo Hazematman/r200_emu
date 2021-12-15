95c75403390c70d51cee3e2ffc547095e2140881
Lucas Fryzek <lucas.fryzek@hazeco.xyz>
2021-12-14 20:04:36 -0500

    Add more packet parsing commands

--- main.cpp
+++ main.cpp
@@ -19,11 +19,17 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+
+/* This files is pulled from the linux kernel */
+#include "r100d.h"
+#include "r200_reg.h"
+
 using namespace std;
 
 #define STR_SIZE(x) sizeof((x)), strdup(x)
 
 #define PCI_CHIP_RV250_If 0x4966
+#define MAX_VERTEX_ARRAYS 16
 
 extern "C"
 {
@@ -83,6 +89,8 @@ static unordered_map<uint32_t, uint32_t> radeon_info =
 };
 
 static vector<uint8_t*> gem_buffers;
+
+//static uint32_t registers[64*1024];
 
 void my_get_resources(struct drm_mode_card_res *o_res)
 {
@@ -168,25 +176,179 @@ void my_gem_mmap(struct drm_radeon_gem_mmap *gem_mmap)
     return;
 }
 
+void my_parse_packets(uint32_t *packets, size_t num_packets)
+{
+    int packet_type = -1;
+    int64_t count;
+    size_t cur_reg;
+    uint32_t op_code;
+    uint8_t *v_addrs[MAX_VERTEX_ARRAYS];
+    for(size_t i = 0; i < num_packets; i++)
+    {
+        uint32_t &val = packets[i];
+        if(packet_type == -1)
+        {
+            switch(val & CP_PACKET3)
+            {
+                case CP_PACKET0:
+                    cur_reg = (val & PACKET0_BASE_INDEX_MASK) << 2;
+                    count = ((val & PACKET0_COUNT_MASK) >> PACKET0_COUNT_SHIFT)+1;
+                    packet_type = 0;
+                    printf("CP0 on 0x%lx with count %lu\n", cur_reg, count);
+                    break;
+                case CP_PACKET1:
+                    printf("Got CP_PACKET1 don't know how to handle this\n");
+                    break;
+                case CP_PACKET2:
+                    /* This is a NOP */
+                    break;
+                case CP_PACKET3:
+                    count = ((val & PACKET3_COUNT_MASK) >> PACKET3_COUNT_SHIFT);
+                    op_code = (val & PACKET3_IT_OPCODE_MASK) >> PACKET3_IT_OPCODE_SHIFT;
+                    packet_type = 3;
+                    printf("Count is %ld\n", count);
+                    break;
+            }
+        }
+        else if(packet_type == 0)
+        {
+            printf("Updating register 0x%08lx with value 0x%08x\n", cur_reg, val);
+            cur_reg += 4;
+            count -= 1;
+            if(count == 0)
+            {
+                packet_type = -1;
+            }
+        }
+        else if(packet_type == 3)
+        {
+            switch(op_code)
+            {
+                case PACKET3_3D_LOAD_VBPNTR:
+                    {
+                        uint32_t num_arrays = packets[i++];
+                        printf("Num arrays %u\n", num_arrays);
+                        for(uint32_t j = 0; j < num_arrays; j++)
+                        {
+                            if((j%2) == 0)
+                            {
+                                printf("Vertex attributes 0x%x\n", packets[i++]);
+                                count -= 1;
+                            }
+                            v_addrs[j] = (uint8_t*)(uintptr_t)packets[i++];
+                            printf("Vertex pointer 0x%lx\n", (size_t)v_addrs[j]);
+                            count -= 1;
+                        }
+
+                        for(uint32_t j = 0; j < num_arrays; j++)
+                        {
+                            /* Reloc should have magic value before it */
+                            if(packets[i++] == 0xc0001000)
+                            {
+                                size_t handle = packets[i++];
+                                v_addrs[j] = &gem_buffers[handle][0] + (uintptr_t)v_addrs[j]; 
+                            }
+                        }
+
+                        /* Decrement i as we would have incremented one more time than we need */
+                        i--;
+                    } 
+                    break;
+                case PACKET3_NOP:
+                    count -= 1;
+                    break;
+                case PACKET3_3D_DRAW_INDX_2:
+                    {
+                        uint32_t ctrl = packets[i++];
+                        uint32_t num_verts = ctrl >> 16;
+                        count--;
+                        if(ctrl & R200_VF_INDEX_SZ_4)
+                        {
+                            printf("Number of draw verticies 32bit %u\n", num_verts);
+                            for(uint32_t j = 0; j < num_verts; j++)
+                            {
+                                i++;
+                                count--;
+                            }
+                        }
+                        else
+                        {
+                            num_verts /= 2;
+                            printf("Number of draw verticies 16bit %u\n", num_verts);
+                            for(uint32_t j = 0; j < num_verts; j++)
+                            {
+                                uint32_t vert = packets[i++];
+                                printf("Vertex indices are %u %u\n", vert>>16, vert);
+                                count--;
+                            }
+                        }
+                    } 
+                    break;
+                default:
+                    printf("Unknown opcode 0x%x\n", op_code);
+                    break;
+            }
+
+            printf("Count is %ld\n", count);
+            if(count <= 0)
+            {
+                packet_type = -1;
+            }
+        }
+    }
+}
+
+void my_parse_relocs(uint32_t *packets, size_t num_packets, uint32_t *relocs, size_t num_relocs)
+{
+    for(size_t i=0; i < num_relocs/4; i++)
+    {
+        struct drm_radeon_cs_reloc *reloc = (struct drm_radeon_cs_reloc*)&relocs[i*4];
+        printf("Reloc handle 0x%x\n", reloc->handle);
+        printf("Reloc read domain 0x%x\n", reloc->read_domains);
+        printf("Reloc write domain 0x%x\n", reloc->write_domain);
+        printf("Reloc flag 0x%x\n", reloc->flags);
+    }
+}
+
 void my_radeon_cs(struct drm_radeon_cs *cs)
 {
     printf("num chunks %u\n", cs->num_chunks);
     printf("cs id %u\n", cs->cs_id);
     struct drm_radeon_cs_chunk **chunk_ptr = (struct drm_radeon_cs_chunk**)cs->chunks;
     printf("Chunks:\n");
+    uint32_t *packets = NULL;
+    size_t num_packets;
+    uint32_t *relocs = NULL;
+    size_t num_relocs;
     for(int i=0; i < (int)cs->num_chunks; i++)
     {
         printf("\t%u %u %llu\n", chunk_ptr[i][0].chunk_id, chunk_ptr[i][0].length_dw, chunk_ptr[i][0].chunk_data);
         if(chunk_ptr[i][0].chunk_id == RADEON_CHUNK_ID_IB)
         {
-            printf("IB buffer\n");
-            uint32_t *packets = (uint32_t*)chunk_ptr[i][0].chunk_data;
-            for(int j = 0; j < (int)chunk_ptr[i][0].length_dw; j++)
+            printf("\tIB buffer\n");
+            packets = (uint32_t*)chunk_ptr[i][0].chunk_data;
+            num_packets = chunk_ptr[i][0].length_dw;
+#if 0
+            for(size_t j = 0; j < num_packets; j++)
             {
-                printf("\t0x%x\n", packets[j]);
+                printf("[0x%04lx] 0x%08x\n", j, packets[j]);
             }
+#endif
+        }
+        else if(chunk_ptr[i][0].chunk_id == RADEON_CHUNK_ID_RELOCS)
+        {
+            printf("\treloc buffer\n");
+            relocs = (uint32_t*)chunk_ptr[i][0].chunk_data;
+            num_relocs = chunk_ptr[i][0].length_dw;
         }
     }
+
+    if(relocs != NULL && packets != NULL)
+        my_parse_relocs(packets, num_packets, relocs, num_relocs);
+
+    if(packets != NULL)
+        my_parse_packets(packets, num_packets);
+
     printf("gart limit %llu\n", cs->gart_limit);
     printf("vram limit %llu\n", cs->vram_limit);
     return;
